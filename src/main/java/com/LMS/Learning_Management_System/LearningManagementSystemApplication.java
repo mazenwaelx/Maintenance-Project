3efd756906eab15aa7045d5753911cc034dc3796
Abdelrhman <abdo.mohsen3457643@gmail.com>
2024-12-22 01:01:07 +0200

    testing course controller and service

--- src/main/java/com/LMS/Learning_Management_System/LearningManagementSystemApplication.java
+++ src/main/java/com/LMS/Learning_Management_System/LearningManagementSystemApplication.java
@@ -1,11 +1,7 @@
 package com.LMS.Learning_Management_System;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
 @SpringBootApplication
-@ComponentScan("com.LMS.Learning_Management_System")
 public class LearningManagementSystemApplication {
 
 	public static void main(String[] args) {
